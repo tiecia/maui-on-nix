4f61515b973de9788996b15d1fbf19366db3b543
Pavel Anpin <pavel@anpin.fyi>
2024-05-08 09:16:22 +0100

    adjusted workaround

--- shell.nix
+++ shell.nix
@@ -9,12 +9,13 @@ let
       # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
 
       postBuild = (previousAttrs.postBuild or '''') + ''
-
-        for i in $out/sdk-manifests/*
-        do
-          i=$(basename $i)
-          mkdir -p $out/metadata/workloads/''${i/-*}
-          touch $out/metadata/workloads/''${i/-*}/userlocal
+         for i in $out/sdk/*; do
+           i=$(basename $i)
+           length=$(printf "%s" "$i" | wc -c)
+           substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+           i="$substring""00"
+           mkdir -p $out/metadata/workloads/''${i/-*}
+           touch $out/metadata/workloads/''${i/-*}/userlocal
         done
       '';
     });
