4bf1f68f48fd6c65145016d5b9d2a4f375cf0f2d
Pavel Anpin <pavel@anpin.fyi>
2024-10-15 16:24:14 -0300

    working android nixos shell with dotnet-9.0-rc2

--- flake.nix
+++ flake.nix
@@ -6,16 +6,21 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs?ref=master";
     };
+
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+    };
+
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, android-nixpkgs , ... }:
     let 
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system ; };
     inherit (inputs.nixpkgs) lib; 
     in {
       
-      devShells.${system}.default = import ./shell.nix {inherit pkgs;};
+      devShells.${system}.default = import ./shell.nix {inherit pkgs  android-nixpkgs; };
 
   };
 }
--- shell.nix
+++ shell.nix
@@ -1,13 +1,19 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }, android-nixpkgs ? import <android-nixpkgs> { } }:
 with pkgs;
 let 
-    dotnet-combined = (with dotnetCorePackages; combinePackages [
-      sdk_9_0
-    ]).overrideAttrs (finalAttrs: previousAttrs: {
-      # This is needed to install workload in $HOME
-      # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
-
-      postBuild = (previousAttrs.postBuild or '''') + ''
+    android-sdk =  android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            
+            build-tools-35-0-0
+            build-tools-34-0-0
+            cmdline-tools-latest
+            emulator
+            platform-tools
+            platforms-android-35
+            platforms-android-34
+          ]);
+    # This is needed to install workload in $HOME
+    # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
+    userlocal =  ''
          for i in $out/sdk/*; do
            i=$(basename $i)
            length=$(printf "%s" "$i" | wc -c)
@@ -17,11 +23,46 @@ let
            touch $out/metadata/workloads/''${i/-*}/userlocal
         done
       '';
+    # append userlocal sctipr to postInstall phase
+    postInstallMap = (finalAttrs: previousAttrs: {
+        postInstall = (previousAttrs.postInstall or '''') + userlocal;
     });
+    # append userlocal sctipr to postBuild phase
+    postBuildMap = (finalAttrs: previousAttrs: {
+        postBuild = (previousAttrs.postBuild or '''') + userlocal;
+    });
+
+    # use this if you don't need multiple SDK versions
+    # dotnet-combined = dotnetCorePackages.sdk_9_0.overrideAttrs postInstallMap)
+    
+    # or use this if you ought to have multiple SDK versions
+    # this will create userlocal files in both $DOTNET_ROOT and dotnet bin realtive path 
+    dotnet-combined = (with dotnetCorePackages; 
+      combinePackages [
+        (sdk_9_0.overrideAttrs postInstallMap)
+        (sdk_8_0.overrideAttrs postInstallMap)
+      ]).overrideAttrs postBuildMap;
+
 in mkShell {
   packages = [
       dotnet-combined
       tree
+      android-sdk
+      gradle
+      jdk17
+      aapt
+      llvm_18
   ];
   DOTNET_ROOT = "${dotnet-combined}";
+  ANDROID_HOME = "${android-sdk}/share/android-sdk";
+  ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
+  JAVA_HOME = jdk17.home;
+  NIX_LD_LIBRARY_PATH= pkgs.lib.makeLibraryPath [ 
+      (lib.getLib pkgs.llvm_18)
+      (lib.getLib pkgs.glibc)
+    ];
+  # make sure you have `programs.nix-ld.enable = true;` in your nixos config
+  NIX_LD = pkgs.runCommand "ld.so" {} ''
+    ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+  '';
 }
